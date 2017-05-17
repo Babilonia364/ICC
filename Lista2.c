a01fc64d31b8ffd67d3d18c3fed3fb1a23a902db
Júnior <Babilonia364@users.noreply.github.com>
2017-05-16 22:51:50 -0300

    Add files via upload

---
+++ Lista2.c
@@ -0,0 +1,229 @@
+#include<stdio.h>
+#include<math.h> //Biblioteca usada para funções matemáticas básicas
+
+void resto () // Calcular o multiplo
+{
+	int num1, resto, i = 0;
+	printf("Digite um numero inteiro que deseja saber se e multiplo de 2, 3 ou 6\n");
+	scanf("%d", &num1);
+	resto = num1%2; // Resto recebe o resto da divisão do número digitado por 2
+	if (resto == 0) //Se o resto for igual a zero
+	{
+		i++; //Incrementando o valor de i em 1, ou seja, i = i + 1
+		printf("Multiplo de 2\n");
+	}
+	else
+	{
+		printf("Nao e multiplo de 2\n");
+	}
+	
+	resto = num1%3;
+	if (resto == 0)
+	{
+		i++; //Análogo ao comentário anterior
+		printf("Multiplo de 3\n");
+	}
+	else
+	{
+		printf("Nao e multiplo de 3\n");
+	}
+	
+	if (i == 2) //Se o i tiver sido incrementado 2 vezes, o número é multiplo de 6
+	{
+		printf("Multiplo de 6\n");
+	}
+	else
+		printf("Nao e multiplo de 6\n");
+	//Não precisa declarar as funções if e else entre colchetes
+	//Mas é bom para ter um código legível
+}
+
+void paridade () // Calcular a paridade
+{
+	int num1, resto;
+	float expo;
+	printf("Digite o numero que deseja saber se e impar ou par\n");
+	scanf("%d", &num1);
+	resto = num1 % 2;
+	
+	if (resto == 0)
+	{
+		expo = pow(num1, 1/2); //Função da biblioteca math.h para calcular raiz e quadrado
+		printf("A raiz e: %.2f\n", expo);
+	}
+	else
+	{
+		expo = pow(num1, 2); // Mesma coisa CARLOS ABOBIM!!!
+		printf("O quadrado do numero e: %.2f\n", expo);
+	}
+}
+
+void media() // Calcular a media
+{
+	double N1, N2, N3, ME;
+	printf("Digite suas 3 bombinhas da prova do thelmo\n");
+	scanf("%lf %lf %lf", &N1, &N2, &N3);
+	ME = (N1 + N2 + N3)/3;
+	ME = (N1 + (2*N2) + (3*N3) + ME)/7;
+	
+	if (ME >= 9)
+	{
+		printf("Conceito A\n");
+	}
+	else //Gambiarra dos if, se senão se, se senão se
+		if (ME>=7.5)
+		{
+			printf("Conceito B\n");
+		}
+		else
+			if (ME>=6)
+			{
+				printf("Conceito C\n");
+			}
+			else
+				if (ME>=4)
+				{
+					printf("Conceito D\n");
+				}
+				else
+					if (ME>=0)
+					{
+						printf("Conceito E\n");
+					}
+}
+
+int calculadora2(int j) //Função secundária da calculadora
+{
+	int num1, num2;
+	printf("Digite 2 valores\n");
+	scanf("%d %d", &num1, &num2);
+	switch (j)
+	{
+		case 0:
+			printf("NAO TEM PORRA\n");
+		break;
+		
+		case 1: //Soma
+			num1 = num1 + num2;
+			printf("A soma e: \n", num1);
+		break;
+		
+		case 2: //Subtração
+			num1 = num1 - num2;
+			printf("A diferenca e: \n", num1);
+		break;
+		
+		case 3: //Multiplicação
+			num1 = num1*num2;
+			printf("A multiplicacao e: \n", num1);
+		break;
+		
+		case 4: //Divisão
+			num1 = num1/num2;
+			printf("A divisao e: \n", num1);
+		break;
+	}
+}
+
+void calculadora1() //Função Primária da calculadora
+{
+	int i, j;
+	printf("Escolha uma das seguintes funções\nSoma digite 1\nSubtraçao digite 2\nMultiplicacao digite 3\nDivisao divide 4\n");
+	scanf("%d", &i);
+	{
+		switch (i)
+		{
+			case 0:
+				printf("SÓ ENTRE ZERO E 1 CARALHUDO");
+			break;
+			
+			case 1: //Soma
+				j=1;
+				calculadora2(j);
+			break;
+			
+			case 2: //Subtração
+				j=2;
+				calculadora2(j);
+			break;
+			
+			case 3: //Multiplicação
+				j=3;
+				calculadora2(j);
+			break;
+			
+			case 4: //Divisão
+				j=4;
+				calculadora2(j);
+			break;
+		}
+	}
+}
+
+void triangulo() // Triângulos Duh
+{
+	int A, B, C;
+	printf("Digite os valores dos 3 lados do triangulo numeros inteiros apenas.\n");
+	scanf("%d %d %d", &A, &B, &C);
+	if (A < B + C) //If aninhado
+	{
+		if (B < C + A)
+		{
+			if (C < A + B)
+			{
+				printf("O triangulo existe");
+			}
+		}
+	}
+	else
+		printf("Nao e triangulo");
+	
+	if (A == B == C)
+	{
+		printf("Triangulo equilatero");
+	}
+	//Não é necessário ter else
+	if (A == B || B == C || A == C) //Operador ou ||
+	{
+		printf("Triangulo isoceles");
+	}
+	if (A != B != C)
+	{
+		printf("Triangulo escaleno");
+	}
+}
+
+int main ()
+{
+	int operador;
+	printf("Resto 1\nParidade 2\nMedia 3\n Calculadora 4\n Triangulo 5\n");
+	scanf("%d", &operador);
+	
+	switch (operador)
+	{
+		case 0:
+			printf("Perdi a partida\n");
+		break;
+		
+		case 1:
+			resto();
+		break;
+		
+		case 2:
+			paridade();
+		break;
+		
+		case 3:
+			media();
+		break;
+		
+		case 4:
+			calculadora1();
+		break;
+		
+		case 5:
+			triangulo();
+		break;
+	}
+	return 0;
+}
